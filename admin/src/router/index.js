0b7488156f4296a05ed9697ad463b76f22439e1d
yellowsea <yellowsea@shu.edu.cn>
2018-05-24 20:05:50 +0800

    frame

---
+++ admin/src/router/index.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import VueCookie from 'vue-cookie'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
+import Course from '@/components/Course'
+import Grades from '@/components/Grades'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/course',
+      name: 'Course',
+      component: Course
+    },
+    {
+      path: '/grades',
+      name: 'Grades',
+      component: Grades
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (!VueCookie.get('id')) {
+    if (to.path === '/login') {
+      next()
+    } else {
+      next('/login')
+    }
+  } else {
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  }
+})
+
+export default router
---
+++ teacher/src/main.js
@@ -0,0 +1,31 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import VueCookie from 'vue-cookie'
+import Axios from 'axios'
+import App from './App'
+import router from './router'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.config.productionTip = true
+
+Vue.use(Vuetify)
+
+Vue.prototype.$axios = Axios.create({
+  baseURL: 'http://jwc.yellowsea.top/api/',
+  headers: {'Content-Type': 'Application/json;charset=utf-8'},
+  withCredentials: true,
+  timeout: 5000
+})
+
+Vue.prototype.$cookie = VueCookie
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
