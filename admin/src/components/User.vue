e992a0f193360d99a37df8a9137b70d73040e9fc
yellowsea <yellowsea@shu.edu.cn>
2018-05-25 13:17:44 +0800

    Bug Fix

--- admin/src/components/User.vue
+++ admin/src/components/User.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="User">
+    <v-toolbar color="cyan" dark tabs>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>用户管理</v-toolbar-title>
+      <v-tabs
+        slot="extension"
+        v-model="model"
+        centered
+        color="cyan"
+        slider-color="yellow"
+      >
+        <v-tab
+          v-for="i in 3"
+          :key="i"
+          :href="`#tab-${i}`"
+        >
+          Item {{ i }}
+        </v-tab>
+      </v-tabs>
+    </v-toolbar>
+    <v-tabs-items v-model="model">
+      <v-tab-item
+        v-for="i in 3"
+        :key="i"
+        :id="`tab-${i}`"
+      >
+        <v-card flat>
+          <v-card-text v-text="text"></v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
   </div>
 </template>
 
